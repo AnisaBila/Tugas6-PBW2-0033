27d42a64698bb4fe68968844910e363a7c8db0a1
AnisaBila <Lynette45>
2024-12-08 16:44:35 +0700

    codeigniter-composer-0033

---
+++ app/Views/data.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Diri</title>
+    <!-- Link ke Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header bg-primary text-white text-center">
+                <h3>Data Diri Mahasiswa</h3>
+            </div>
+            <div class="card-body">
+                <div class="row mb-3">
+                    <div class="col-md-2 font-weight-bold">Nama</div>
+                    <div class="col-md-8">: Anisa Bila Septiana</div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-2 font-weight-bold">NIM</div>
+                    <div class="col-md-8">: 23.240.0033</div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-2 font-weight-bold">Kelas</div>
+                    <div class="col-md-8">: 3P41</div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-2 font-weight-bold">Program Studi</div>
+                    <div class="col-md-8">: Teknik Informatika</div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-2 font-weight-bold">Jenjang</div>
+                    <div class="col-md-8">: S1</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="text-black text-center mt-1">
+        <p>&copy; 23.240.0033 - Anisa Bila Septiana.</p>
+    </footer>
+
+</body>
+</html>
